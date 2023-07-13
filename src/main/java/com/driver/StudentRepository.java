bff4d9b618cc8d21873cdc656a19a47252dbd510
Shubham Boke <shubhamboke.99@gmail.com>
2023-07-13 18:15:10 +0530

    first commit

---
+++ src/main/java/com/driver/StudentRepository.java
@@ -0,0 +1,31 @@
+package com.driver;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+@Repository
+public class StudentRepository {
+
+    private List<Student> studentList = new ArrayList<>();
+    private List<Teacher> teacherList = new ArrayList<>();
+    private HashMap<Teacher, ArrayList<Student>> underTeacherList = new HashMap<>();
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+    public List<Teacher> getTeacherList(){
+        return teacherList;
+    }
+
+    public HashMap<Teacher, ArrayList<Student>> getStudentTeacherMap() {
+        return underTeacherList;
+    }
+
+    public void removeTeacher(Teacher teacher) {
+        underTeacherList.remove(teacher);
+    }
+}
---
+++ src/main/java/com/driver/StudentService.java
@@ -0,0 +1,129 @@
+package com.driver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    public void addStudent(Student student) {
+        List<Student> studentList = studentRepository.getStudentList();
+        studentList.add(student);
+    }
+
+    public void addTeacher(Teacher teacher) {
+        List<Teacher> teacherList = studentRepository.getTeacherList();
+        teacherList.add(teacher);
+    }
+
+    public void addStudentTeacherPair(String student, String teacher) {
+        List<Student> studentList = studentRepository.getStudentList();
+        List<Teacher> teacherList = studentRepository.getTeacherList();
+        Student stu = null;
+        Teacher tec = null;
+        for(Student s: studentList){
+            if(s.getName().equals(student)){
+                stu = s;
+                break;
+            }
+        }
+        for(Teacher t: teacherList){
+            if(t.getName().equals(teacher)){
+                tec = t;
+                break;
+            }
+        }
+
+        HashMap<Teacher, ArrayList<Student>> STMap = studentRepository.getStudentTeacherMap();
+        ArrayList<Student> studentListUnderTeacher = STMap.getOrDefault(tec, new ArrayList<>());
+        studentListUnderTeacher.add(stu);
+        STMap.put(tec, studentListUnderTeacher);
+        System.out.println(studentListUnderTeacher);
+        tec.setNumberOfStudents(tec.getNumberOfStudents()+1);
+    }
+
+    public Student getStudent(String name) {
+        List<Student> studentList = studentRepository.getStudentList();
+        for(Student s: studentList){
+            if(s.getName().equals(name)){
+                return s;
+            }
+        }
+        return null;
+    }
+
+    public Teacher getTeacher(String name) {
+        List<Teacher> teacherList = studentRepository.getTeacherList();
+        for(Teacher t: teacherList){
+            if(t.getName().equals(name)){
+                return t;
+            }
+        }
+        return null;
+    }
+
+    public List<String> getStudentNameList(String teacherName) {
+        List<Teacher> teacherList = studentRepository.getTeacherList();
+        Teacher teacher = null;
+        for(Teacher t: teacherList){
+            if(t.getName().equals(teacherName)){
+                teacher = t;
+                break;
+            }
+        }
+        HashMap<Teacher, ArrayList<Student>> STMap = studentRepository.getStudentTeacherMap();
+        List<Student> studentListUnderTeacher = STMap.getOrDefault(teacher, new ArrayList<>());
+        List<String> res = new ArrayList<>();
+        for(Student s: studentListUnderTeacher){
+            res.add(s.getName());
+        }
+        return res;
+    }
+
+    public List<String> getAllStudentNameList() {
+        List<Student> studentList = studentRepository.getStudentList();
+        List<String> res = new ArrayList<>();
+        for(Student s: studentList){
+            res.add(s.getName());
+        }
+        return res;
+    }
+
+    public void deleteTeacherWithHerStudents(String teacherName) {
+        List<Student> studentList = studentRepository.getStudentList();
+        List<Teacher> teacherList = studentRepository.getTeacherList();
+        Teacher teacher = null;
+        for(Teacher t: teacherList){
+            if(t.getName().equals(teacherName)){
+                teacher = t;
+                break;
+            }
+        }
+        HashMap<Teacher, ArrayList<Student>> STMap = studentRepository.getStudentTeacherMap();
+        List<Student> studentListUnderTeacher = STMap.getOrDefault(teacher, new ArrayList<>());
+
+        for(Student s: studentListUnderTeacher){
+            studentList.remove(s);
+        }
+        teacherList.remove(teacher);
+        studentRepository.removeTeacher(teacher);
+    }
+
+    public void deleteAllTeachersWithHerStudents() {
+        List<Teacher> teacherList = studentRepository.getTeacherList();
+        List<Teacher> teacherListCopy = new ArrayList<>();
+        for(Teacher t: teacherList){
+            teacherListCopy.add(t);
+        }
+        for(Teacher t: teacherListCopy){
+            deleteTeacherWithHerStudents(t.getName());
+        }
+    }
+}
